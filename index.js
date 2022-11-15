5a1d7bb745bcf6529c0c04b1ec59e46ee1ab07f8
Santiago Garcia <sgcarvallo@gmail.com>
2022-11-15 18:06:13 -0500

    Finished the project
    Finished project finally. Probably my worst time so far in this project.
    Glad to be done with it and grateful for the help I got from my friends.

--- index.js
+++ index.js
@@ -1,43 +1,62 @@
-// defining options
+// defining variables
 
 let choices = ['rock', 'paper', 'scissors'];
+let userScore = 0;
+let computerScore = 0;
 
 // getting choices from computer and player
 
 function getComputerChoice() {
     let computerChoice = choices[Math.floor(Math.random() * choices.length)];
     return computerChoice;
+    
 }
 
 function getUserChoice() {
     let userChoice = prompt('Choose your weapon');
-    return userChoice;
+    return userChoice.toLowerCase();
+    
 }
 
-function playRound(userChoice, computerChoice) {
-    // check to see if round is a tie
-    if (userChoice === computerChoice) {
-        return "It's a draw."
+function runWholeGame () {
+    for (let i = 0; i < 5; i++){
+        playRound();        
+    }   
+    console.log("Game has ended!");
+}
+
+
+function playRound() {   
+    
+
+    let computerChoice = getComputerChoice();
+    let userChoice = getUserChoice();
+    console.log(userChoice, computerChoice);
+    console.log(userScore);
+    console.log(computerScore);
+
+    // check answers/tie
+    if (userChoice === computerChoice){
+        console.log("It's a tie!")
+        return;
     }
 
-    // checking if game
+    // checking for winner
     const weaponOfChoice = {
         rock: 'scissors',
         paper: 'rock',
         scissors: 'paper'
     }
-    return weaponOfChoice[userChoice] === computerChoice ? 'You win!' : 'Computer wins!'
-}
 
-function game(){
-
-    
-    let computerWins = 0;
-
-    for (let playerWins = 0; playerWins < 5; playerWins++) {
-        // your code here!
-        
+    if (weaponOfChoice[userChoice] === computerChoice){
+        userScore++
+    } else {
+        computerScore++
     }
+
+    console.log(weaponOfChoice[userChoice] === computerChoice ? `You win! ${userChoice} beats ${computerChoice}` : `Computer wins! ${computerChoice} beats ${userChoice}`);
+
 }
 
-console.log(playRound(getUserChoice(), getComputerChoice()));
+
+runWholeGame();
